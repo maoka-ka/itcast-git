48ae32da8d737f76bea75b1d9d1c4bc581b5208c
zhengdongbing <971393841@qq.com>
2020-04-19 19:50:38 +0800

    协同者添加了一行

--- src/main/java/cn/itcast/git/demo/GitDemo.java
+++ src/main/java/cn/itcast/git/demo/GitDemo.java
@@ -13,6 +13,6 @@ public class GitDemo {
         System.out.println("=========clone git2===========");
         System.out.println("=========clone git3===========");
         System.out.println("=========clone git4===========");
-
+        System.out.println("====协同者添加了一行=======");
     }
 }
