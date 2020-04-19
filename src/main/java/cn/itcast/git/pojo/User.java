9736b437897baab5c6ecaf4150b3dc1687d032da
zhengdongbing <971393841@qq.com>
2020-04-19 21:52:01 +0800

    添加了一个文件夹和一个实体类

---
+++ src/main/java/cn/itcast/git/pojo/User.java
@@ -0,0 +1,55 @@
+package cn.itcast.git.pojo;
+
+/**
+ * @Author DongBin Zheng
+ * @Date 2020/4/19 21:50
+ * @Version 1.0
+ */
+public class User {
+    private Integer id;
+    private String name;
+
+    public User() {
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "User{id = " + id + ", name = " + name + "}";
+    }
+}
