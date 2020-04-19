19f1fa94d934f4ba0bee8db80c672311289c5609
zhengdongbing <971393841@qq.com>
2020-04-19 19:52:03 +0800

    我添加了一行

--- src/main/java/cn/itcast/git/demo/GitDemo.java
+++ src/main/java/cn/itcast/git/demo/GitDemo.java
@@ -13,6 +13,6 @@ public class GitDemo {
         System.out.println("=========clone git2===========");
         System.out.println("=========clone git3===========");
         System.out.println("=========clone git4===========");
-
+        System.out.println("==我也添加了一行=====");
     }
 }
